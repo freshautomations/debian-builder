c3335e2c6710000e65684191c798c28f82fb4631
Greg Szabo <greg@philosobear.com>
2023-02-12 11:30:36 -0500

    menu item fixes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Debian-builder
 Utility to embed the `preseed.cfg` file into the Debian NetInstall image.
 
-# Overview
+## Overview
 RedHat has Kickstart, SuSE has AutoYaST and [Debian](https://wiki.debian.org/AutomatedInstallation) has a `preseed.cfg`
 file to automate operating system installations. You can either attach a second drive to your machine while installing
 and manually tell the installer where the file is located, or you can burn the `preseed.cfg` file into the ISO image.
@@ -11,7 +11,7 @@ All the options are slightly cumbersome. This utility automates the process of e
 You just have to [write your own preseed](https://wiki.debian.org/DebianInstaller/Preseed#Default_preseed_files). And
 know how to run Docker...
 
-# How to use
+## How to use
 After [installing Docker](https://docs.docker.com/desktop/) to your favorite operating system and
 [creating your preseed](https://wiki.debian.org/DebianInstaller/Preseed#Default_preseed_files) file, you run the following
 command in the folder where you put your `preseed.cfg`.
@@ -27,8 +27,27 @@ you will see a few new files in your file system:
 Burn the custom ISO to a CD, DVD or USB stick with your favorite application. My current favorite is [balenaEtcher](https://www.balena.io/etcher)
 because it works so well on my macOS.
 
-# Custom configuration
+## Custom configuration
 * If you already have the official ISO downloaded in your folder, it will automatically will be used and it will not be re-downloaded.
 In this case it's the user's responsibility to make sure that the ISO is valid.
 * You can add `-e VERSION=11.6.0` environment variable to the docker execution to download a different Debian version.
 * You can add `-e NAME=custom` environment variable that will be used to name the output image. (Well the last section of it.)
+* You can add `-e TIMEOUT=5` environment variable that will be used to set the bootloader menu timeout _in seconds_.
+
+## I don't have Docker
+Docker is only used to simplify the environment setup. You can run the embedded script without Docker, if you set up your
+own environment. The main components:
+* Install [`xorriso`](https://www.gnu.org/software/xorriso/) in your environment. This is used unpack/repack the ISO image.
+* Download the Official Debian Netinstall image or have `wget`, `sha512sum` and `gnupg` installed so the script can
+download and verify it for you.
+* Have a generic set of Linux commands available: `cpio`, `sed`, `md5sum`, `gzip`/`gunzip`, `find`, `dd`.
+
+Additional environment variables are available to configure the script:
+* `DEBUG=1` will enable tracing the script line-by-line as well as not removing some of the temporary files.
+* `MNT=/mnt` will tell the script where the input files (preseed.cfg and optionally an official ISO image) are located
+on the file system
+* `MEDIA=/media` will tell the script where to expand the official ISO image.
+* `TMP=/tmp` will mount the temporary folder somewhere else.
+* `FORCE=1` (not set, only used for troubleshooting) will force ALL the boot menu items to be automated installations
+with the `preseed.cfg` set. If this is not set, only the "Automated ..." items are set and the boot menu defaults to
+the automated graphical installation.
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,54 +6,167 @@ if [ -n "${DEBUG:-}" ]; then
   set -x
 fi
 
-# Check if a volume was mounted with preseed.cfg.
-if [ ! -f "/mnt/preseed.cfg" ]; then
-  echo "Error: missing /mnt/preseed.cfg. Please attach a volume to /mnt." 1>&2
-  exit 1
-fi
+# Input defaults and script variables
+NAME="${NAME:-custom}"
+MNT="${MNT:-/mnt}"
+MEDIA="${MEDIA:-/media}"
+TMP="${TMP:-/tmp}"
+DEBUG="${DEBUG:-}"
+FORCE="${FORCE:-}"
+TIMEOUT="${TIMEOUT:-5}"
 
-# Download ISO
-if [ ! -f "/mnt/debian-$VERSION-amd64-netinst.iso" ]; then
-  echo "Downloading https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-$VERSION-amd64-netinst.iso"
-  wget -O "/mnt/debian-$VERSION-amd64-netinst.iso" "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-$VERSION-amd64-netinst.iso"
-  wget -O "/mnt/SHA512SUMS" "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS"
-  wget -O "/mnt/SHA512SUMS.sign" "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS.sign"
-  cd /mnt
+# Check input parameters and existence of preseed.cfg.
+check_input() {
+  if [ -z "${VERSION+set}" ]; then
+    echo "Error: VERSION is not set. Please set a Debian ISO version number."
+    exit 1
+  fi
+  if [ ! -f "${MNT}/preseed.cfg" ]; then
+    echo "Error: missing ${MNT}/preseed.cfg. Please attach a volume to ${MNT}." 1>&2
+    exit 1
+  fi
+}
+
+# Download official Debian netinstall ISO or do nothing if one is already found on the volume.
+get_iso() {
+if [ ! -f "${MNT}/debian-${VERSION}-amd64-netinst.iso" ]; then
+  echo "Downloading https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-${VERSION}-amd64-netinst.iso"
+  wget -O "${MNT}/debian-${VERSION}-amd64-netinst.iso" "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-${VERSION}-amd64-netinst.iso"
+  wget -O "${MNT}/SHA512SUMS" "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS"
+  wget -O "${MNT}/SHA512SUMS.sign" "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS.sign"
+  cd "${MNT}"
   sha512sum --ignore-missing -c SHA512SUMS
   gpg --keyserver keyring.debian.org --auto-key-retrieve --verify SHA512SUMS.sign SHA512SUMS
-  rm SHA512SUMS.sign SHA512SUMS
+  if [ -z "${DEBUG}" ]; then
+    rm SHA512SUMS.sign SHA512SUMS
+  fi
 else
-  echo "Using existing ISO at /mnt/debian-$VERSION-amd64-netinst.iso"
-fi 
+  echo "Using existing ISO at ${MNT}/debian-${VERSION}-amd64-netinst.iso"
+fi
+}
 
 # Unpack ISO
-xorriso -osirrox on -indev "/mnt/debian-$VERSION-amd64-netinst.iso" -extract . /media
+unpack_iso() {
+  xorriso -osirrox on -indev "${MNT}/debian-${VERSION}-amd64-netinst.iso" -extract . "${MEDIA}"
+}
 
-# Add preseed to all "initrd"s
-for i in $(find /media/install.amd -follow -type f -name initrd.gz)
-do
-  #Do the deed
-  gunzip "$i"
-  NOGZ="${i%%.gz}"
-  echo /mnt/preseed.cfg | cpio -H newc -o -A -F "$NOGZ"
-  gzip "$NOGZ"
-  #Update md5sum
-  LISTNAME=".${i##/media}"
-  MD5SUM="$(md5sum "$LISTNAME")"
-  sed -i "s,^[0-9a-f]*  ${LISTNAME}$,${MD5SUM}," "/media/md5sum.txt"
-done
+# Update the MD5 file for existing files. Input: absolute path (Starting with ${MEDIA}).
+update_md5() {
+      LISTNAME=".${1##${MEDIA}}"
+      MD5SUM="$(cd "${MEDIA}" && md5sum "${LISTNAME}")"
+      sed -i "s,^[0-9a-f]*  ${LISTNAME}$,${MD5SUM}," "${MEDIA}/md5sum.txt"
+}
+
+# Add preseed to all "initrd"s.
+# Slightly more convoluted than just copying it to the file system.
+# During installation, it is mounted under /mnt/preseed.cfg.
+preseed_into_initrd() {
+  for i in $(find "${MEDIA}/install.amd" -follow -type f -name initrd.gz)
+  do
+    gunzip "$i"
+    NOGZ="${i%%.gz}"
+    echo "${MNT}/preseed.cfg" | cpio -H newc -o -A -F "$NOGZ"
+    gzip "$NOGZ"
+
+    update_md5 "$i"
+  done
+  PRESEED="file=file:///mnt/preseed.cfg"
+}
+
+# Add preseed onto the ISO filesystem. Simple solution.
+# During installation, it is mounted under /cdrom/preseed.cfg.
+preseed_onto_fs() {
+  cp "${MNT}/preseed.cfg" "${MEDIA}"
+  cd "${MEDIA}" && md5sum "preseed.cfg" >> "${MEDIA}/md5sum.txt"
+  PRESEED="file=file:///cdrom/preseed.cfg"
+}
+
+# Set default menu entry for UEFI boot
+bios_set_default_entry() {
+  # FORCE=no: We will add preseed.cfg to only the "Automated install" entries in the boot menu.
+  if [ -z "${FORCE}" ]; then
+    # Remove menu default choices.
+    grep -l "menu default" "${MEDIA}"/isolinux/*.cfg | while IFS= read -r line
+    do
+      sed -i '/^\tmenu default$/d' "$line"
+      sed -i '/^default \(.*\)$/d' "$line"
+      update_md5 "$line"
+    done
+    # Add default choice for automated (label auto*) menu items.
+    grep -l "label auto" "${MEDIA}"/isolinux/*.cfg | while IFS= read -r line
+    do
+        MENUITEM="$(grep "label auto" "$line" | sed 's/.*label //')"
+        sed -i '/label '"${MENUITEM}"'/ a default '"${MENUITEM}"'\n    menu default' "$line"
+        update_md5 "$line"
+    done
+    # Open boot menu to advanced options
+    sed -i '/^menu begin advanced$/ a menu start' "${MEDIA}"/isolinux/menu.cfg
+    update_md5 "${MEDIA}"/isolinux/menu.cfg
+    # Add preseed.cfg to automatic installation menu items.
+    APPEND="${PRESEED}"
+    grep -l "auto=true" "${MEDIA}"/isolinux/*.cfg | while IFS= read -r line
+    do
+      sed -i 's@auto=true@'"$APPEND auto=true"'@' "$line"
+      update_md5 "$line"
+    done
+  # FORCE=yes: We will add preseed.cfg to all the entries in the boot menu.
+  else
+    APPEND="${PRESEED} auto=true" #Note: in some menu items "auto=true" will show twice. That's ok.
+    for i in "${MEDIA}"/isolinux/*.cfg
+    do
+      sed -i 's@\---@'"$APPEND ---"'@' "$i"
+      update_md5 "$i"
+    done
+  fi
+  # Set boot menu timeout.
+  sed -i 's/^timeout .*$/timeout '"${TIMEOUT}0"'/' "${MEDIA}"/isolinux/isolinux.cfg
+  update_md5 "${MEDIA}"/isolinux/isolinux.cfg
+}
+
+uefi_set_default_entry() {
+  # FORCE=no: We will add preseed.cfg to only the "Automated install" entries in the boot menu.
+  if [ -z "${FORCE}" ]; then
+    # Add preseed.cfg to automatic installation menu items.
+    APPEND="${PRESEED}"
+    sed -i 's@auto=true@'"$APPEND auto=true"'@' "${MEDIA}"/boot/grub/grub.cfg
+    # Set default entry and timeout
+    sed -i '/insmod play/ a default='\'"2"\''\ntimeout='"${TIMEOUT}"'' "${MEDIA}"/boot/grub/grub.cfg
+    sed -i '/submenu.*Advanced options/ a default=2' "${MEDIA}"/boot/grub/grub.cfg
+    sed -i '/submenu.*Speech-enabled advanced options/ a default=2' "${MEDIA}"/boot/grub/grub.cfg
+    sed -i '/submenu.*Accessible dark contrast installer menu/ a default=2' "${MEDIA}"/boot/grub/grub.cfg
+    update_md5 "${MEDIA}"/boot/grub/grub.cfg
+  # FORCE=yes: We will add preseed.cfg to all the entries in the boot menu.
+  else
+    APPEND="${PRESEED} auto=true" #Note: in some menu items "auto=true" will show twice. That's ok.
+    sed -i 's@\---@'"$APPEND ---"'@' "${MEDIA}"/boot/grub/grub.cfg
+    sed -i '/insmod play/ a timeout='"${TIMEOUT}"'' "${MEDIA}"/boot/grub/grub.cfg
+    update_md5 "${MEDIA}"/boot/grub/grub.cfg
+  fi
+}
 
 # Build ISO using the previous build command with some simplifications.
-dd if="/mnt/debian-$VERSION-amd64-netinst.iso" bs=1 count=432 of="/tmp/isohdpfx.bin"
-sed \
-    -e 's/-jigdo-[^ ]* [^ ]* //g' \
-    -e 's/-checksum_algorithm_iso [^ ]* //' \
-    -e 's/-checksum-list [^ ]* //' \
-    -e 's,-isohybrid-mbr [^ ]* ,-isohybrid-mbr /tmp/isohdpfx.bin ,' \
-    -e 's/ CD1//' \
-    -e 's, boot1, /media,' \
-    -e 's,-o [^ ]*,-o "/mnt/debian-'"$VERSION"'-amd64-'"$NAME"'.iso",' < /media/.disk/mkisofs > /tmp/mkiso
-chmod 755 /tmp/mkiso
-/tmp/mkiso
+build_iso() {
+  dd if="${MNT}/debian-${VERSION}-amd64-netinst.iso" bs=1 count=432 of="${TMP}/isohdpfx.bin"
+  sed \
+      -e 's/-jigdo-[^ ]* [^ ]* //g' \
+      -e 's/-checksum_algorithm_iso [^ ]* //' \
+      -e 's/-checksum-list [^ ]* //' \
+      -e 's,-isohybrid-mbr [^ ]* ,-isohybrid-mbr '"${TMP}"'/isohdpfx.bin ,' \
+      -e 's/ CD1//' \
+      -e 's, boot1, '"${MEDIA}"',' \
+      -e 's,-o [^ ]*,-o "'"${MNT}"'/debian-'"${VERSION}"'-amd64-'"${NAME}"'.iso",' < "${MEDIA}/.disk/mkisofs" > "${TMP}/mkiso"
+  chmod 755 "${TMP}/mkiso"
+  "${TMP}/mkiso"
+  if [ -z "${DEBUG}" ]; then
+    rm "${TMP}/mkiso"
+  fi
+}
 
-echo "debian-$VERSION-amd64-$NAME.iso created."
+check_input
+get_iso
+unpack_iso
+preseed_onto_fs
+bios_set_default_entry
+uefi_set_default_entry
+build_iso
+echo "debian-${VERSION}-amd64-${NAME}.iso created."
